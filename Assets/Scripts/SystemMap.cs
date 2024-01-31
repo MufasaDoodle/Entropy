efd988f023e511f5fb479a9a6688f7a92aa596b4
Christian Sejer <christian_sejer@hotmail.com>
2024-01-31 18:09:25 +0100

    added temporary planet image and map zooming

--- Assets/Scripts/SystemMap.cs
+++ Assets/Scripts/SystemMap.cs
@@ -7,14 +7,40 @@ public partial class SystemMap : MeshInstance2D
 
 	bool isDragging = false;
 	Vector2 offset = new Vector2(0, 0);
+	int currentZoomLevel;
+	int minZoomLevel = 1;
+	int maxZoomLevel = 100;
+
+	public override void _Ready()
+	{
+		currentZoomLevel = minZoomLevel;
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		if (isDragging)
 		{
-			Position = GetGlobalMousePosition() - offset;
+			DoDrag();
 		}
+	}
+
+	private void DoDrag()
+	{
+		//trying to clamp the map to the edges of the screen
+		/*
+		var screenSize = GetViewportRect().Size;
+		var mapSize = Texture.GetSize();
+
+		Vector2 newPos = GetGlobalMousePosition() - offset;
+
+		newPos.X = Mathf.Clamp(newPos.X, mapSize.X, screenSize.X - mapSize.X);
+		newPos.Y = Mathf.Clamp(newPos.Y, mapSize.Y, screenSize.Y - mapSize.Y);
+
+		Position = newPos;
+		*/
+
+		Position = GetGlobalMousePosition() - offset; 
 	}
 
 	public void OnInputEvent(Node viewport, InputEvent evt, int shape_idx)
@@ -32,6 +58,28 @@ public partial class SystemMap : MeshInstance2D
 				else
 				{
 					isDragging = false;
+				}
+			}
+			else if (mouseEvent.ButtonIndex == MouseButton.WheelDown)
+			{
+				if(currentZoomLevel > minZoomLevel)
+				{
+					currentZoomLevel--;
+					Vector2 scale = Scale;
+					scale.X -= 0.1f;
+					scale.Y -= 0.1f;
+					Scale = scale;
+				}
+			}
+			else if (mouseEvent.ButtonIndex == MouseButton.WheelUp)
+			{
+				if (currentZoomLevel < maxZoomLevel)
+				{
+					currentZoomLevel++;
+					Vector2 scale = Scale;
+					scale.X += 0.1f;
+					scale.Y += 0.1f;
+					Scale = scale;
 				}
 			}
 		}
